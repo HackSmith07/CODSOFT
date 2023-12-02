5b1f982050af56a3f52d4fda125d371819be9d89
HackSmith07 <prathameshsakhare020@gmail.com>
2023-12-02 12:38:41 +0530

    updated

--- potfolio/app.js
+++ potfolio/app.js
@@ -25,3 +25,50 @@ function scrollHeader() {
 }
 window.addEventListener('scroll', scrollHeader);
 
+
+const sections = document.querySelectorAll('section[id]');
+
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+
+    sections.forEach(section => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+
+        let sectionId = section.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.menu a[href *=' + sectionId + ']').classList.add('active-link');
+        } else{
+            document.querySelector('.menu a[href *=' + sectionId + ']').classList.remove('active-link');
+
+
+        }
+
+    })
+}
+
+window.addEventListener('scroll',scrollActive);
+
+const pages = document.querySelectorAll('.page');
+const resume = document.querySelector('.resume');
+
+function resumeActive() {
+    const scrollY = window.pageYOffset;
+
+    pages.forEach(page =>{
+        const sectionHeight = page.offsetHeight;
+        const sectionTop = page.offsetTop - 120;
+        let sectionId = page.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.add('current');
+        } else{
+            document.querySelector('.resume_tabs a[href *=' + sectionId + ']').classList.remove('current');
+        }
+
+    })
+}
+
+window.addEventListener('scroll', resumeActive);
